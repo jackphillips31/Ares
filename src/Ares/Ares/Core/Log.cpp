fb24a8b84d4421dfc7c80ec3e2fa807022999050
Jack Phillips <jack.p.phillips@gmail.com>
2024-11-10 14:58:07 -0700

    Added log files

---
+++ src/Ares/Ares/Core/Log.cpp
@@ -0,0 +1,20 @@
+#include <arespch.h>
+#include "Ares/Core/Log.h"
+
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+namespace Ares {
+	Ref<spdlog::logger> Log::s_CoreLogger;
+	Ref<spdlog::logger> Log::s_ClientLogger;
+
+	void Log::Init(spdlog::level setLevel)
+	{
+		spdlog::set_pattern("%^[%T] %7n: %v%$");
+		s_CoreLogger = spdlog::stdout_color_mt("ARES");
+		s_CoreLogger->set_level(setLevel);
+		s_ClientLogger = spdlog::stderr_color_mt("APP");
+		s_ClientLogger->set_level(setLevel);
+
+		AR_CORE_INFO("Logging initialized!");
+	}
+}
---
+++ src/Ares/Ares/Core/Log.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include "Ares/Core/Core.h"
+
+#include <spdlog/spdlog.h>
+#include <spdlog/fmt/ostr.h>
+#include <spdlog/fmt/fmt.h>
+
+namespace Ares {
+
+	class Log
+	{
+	public:
+		static void Init(spdlog::level setLevel);
+
+		inline static Ref<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static Ref<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+	private:
+		static Ref<spdlog::logger> s_CoreLogger;
+		static Ref<spdlog::logger> s_ClientLogger;
+	};
+
+}
+
+	// Add AR_LOG_LOCATION to Preprocessor Definitions
+	// to enable printing the source location information
+	// from which the log is called.
+
+namespace LogDebug {
+
+	template<typename T>
+	inline static std::string ToString(T& t)
+	{
+		std::stringstream ss;
+		ss << t;
+		return ss.str();
+	}
+
+	template<typename... Args>
+	inline static std::string ToString(const std::string& fmt, Args&&... args)
+	{
+		return fmt::format(fmt, std::forward<Args>(args)...);
+	}
+
+	inline static std::string ExtractFileName(const char* fullFilePath)
+	{
+		std::string filePath(fullFilePath);
+		size_t pos = filePath.find_last_of("/\\");
+		if (pos != std::string::npos)
+		{
+			return filePath.substr(pos + 1);
+		}
+		return filePath;
+	}
+
+	inline static std::string FormatFileAndLine(std::string fileName, int line)
+	{
+		return fmt::format("({}:{})", fileName, line);
+	}
+
+}
+
+#ifdef AR_LOG_LOCATION
+	#define AR_CORE_CRITICAL(...) ::Ares::Log::GetCoreLogger()->critical("{:^40} {:>30}: {}", __FUNCTION__, LogDebug::FormatFileAndLine(LogDebug::ExtractFileName(__FILE__), __LINE__), LogDebug::ToString(__VA_ARGS__))
+	#define AR_CORE_ERROR(...) ::Ares::Log::GetCoreLogger()->error("{:^40} {:>30}: {}", __FUNCTION__, LogDebug::FormatFileAndLine(LogDebug::ExtractFileName(__FILE__), __LINE__), LogDebug::ToString(__VA_ARGS__))
+	#define AR_CORE_WARN(...) ::Ares::Log::GetCoreLogger()->warn("{:^40} {:>30}: {}", __FUNCTION__, LogDebug::FormatFileAndLine(LogDebug::ExtractFileName(__FILE__), __LINE__), LogDebug::ToString(__VA_ARGS__))
+	#define AR_CORE_INFO(...) ::Ares::Log::GetCoreLogger()->info("{:^40} {:>30}: {}", __FUNCTION__, LogDebug::FormatFileAndLine(LogDebug::ExtractFileName(__FILE__), __LINE__), LogDebug::ToString(__VA_ARGS__))
+	#define AR_CORE_BUG(...) ::Ares::Log::GetCoreLogger()->debug("{:^40} {:>30}: {}", __FUNCTION__, LogDebug::FormatFileAndLine(LogDebug::ExtractFileName(__FILE__), __LINE__), LogDebug::ToString(__VA_ARGS__))
+	#define AR_CORE_TRACE(...) ::Ares::Log::GetCoreLogger()->trace("{:^40} {:>30}: {}", __FUNCTION__, LogDebug::FormatFileAndLine(LogDebug::ExtractFileName(__FILE__), __LINE__), LogDebug::ToString(__VA_ARGS__))
+
+	#define AR_CRITICAL(...) ::Ares::Log::GetClientLogger()->critical("{:^40} {:>30}: {}", __FUNCTION__, LogDebug::FormatFileAndLine(LogDebug::ExtractFileName(__FILE__), __LINE__), LogDebug::ToString(__VA_ARGS__))
+	#define AR_ERROR(...) ::Ares::Log::GetClientLogger()->error("{:^40} {:>30}: {}", __FUNCTION__, LogDebug::FormatFileAndLine(LogDebug::ExtractFileName(__FILE__), __LINE__), LogDebug::ToString(__VA_ARGS__))
+	#define AR_WARN(...) ::Ares::Log::GetClientLogger()->warn("{:^40} {:>30}: {}", __FUNCTION__, LogDebug::FormatFileAndLine(LogDebug::ExtractFileName(__FILE__), __LINE__), LogDebug::ToString(__VA_ARGS__))
+	#define AR_INFO(...) ::Ares::Log::GetClientLogger()->info("{:^40} {:>30}: {}", __FUNCTION__, LogDebug::FormatFileAndLine(LogDebug::ExtractFileName(__FILE__), __LINE__), LogDebug::ToString(__VA_ARGS__))
+	#define AR_BUG(...) ::Ares::Log::GetClientLogger()->debug("{:^40} {:>30}: {}", __FUNCTION__, LogDebug::FormatFileAndLine(LogDebug::ExtractFileName(__FILE__), __LINE__), LogDebug::ToString(__VA_ARGS__))
+	#define AR_TRACE(...) ::Ares::Log::GetClientLogger()->trace("{:^40} {:>30}: {}", __FUNCTION__, LogDebug::FormatFileAndLine(LogDebug::ExtractFileName(__FILE__), __LINE__), LogDebug::ToString(__VA_ARGS__))
+#else
+	#define AR_CORE_CRITICAL(...) ::Ares::Log::GetCoreLogger()->critical(LogDebug::ToString(__VA_ARGS__))
+	#define AR_CORE_ERROR(...) ::Ares::Log::GetCoreLogger()->error(LogDebug::ToString(__VA_ARGS__))
+	#define AR_CORE_WARN(...) ::Ares::Log::GetCoreLogger()->warn(LogDebug::ToString(__VA_ARGS__))
+	#define AR_CORE_INFO(...) ::Ares::Log::GetCoreLogger()->info(LogDebug::ToString(__VA_ARGS__))
+	#define AR_CORE_DEBUG(...) ::Ares::Log::GetCoreLogger()->debug(LogDebug::ToString(__VA_ARGS__))
+	#define AR_CORE_TRACE(...) ::Ares::Log::GetCoreLogger()->trace(LogDebug::ToString(__VA_ARGS__))
+
+	#define AR_CRITICAL(...) ::Ares::Log::GetClientLogger()->critical(LogDebug::ToString(__VA_ARGS__))
+	#define AR_ERROR(...) ::Ares::Log::GetClientLogger()->error(LogDebug::ToString(__VA_ARGS__))
+	#define AR_WARN(...) ::Ares::Log::GetClientLogger()->warn(LogDebug::ToString(__VA_ARGS__))
+	#define AR_INFO(...) ::Ares::Log::GetClientLogger()->info(LogDebug::ToString(__VA_ARGS__))
+	#define AR_DEBUG(...) ::Ares::Log::GetClientLogger()->debug(LogDebug::ToString(__VA_ARGS__))
+	#define AR_TRACE(...) ::Ares::Log::GetClientLogger()->trace(LogDebug::ToString(__VA_ARGS__))
+#endif
