f77d47922d2097f3357f98f21e3da3cbd5f926e4
Jack Phillips <jack.p.phillips@gmail.com>
2024-11-10 15:25:25 -0700

    Updated main Ares header

--- src/Ares/Ares.h
+++ src/Ares/Ares.h
@@ -3,5 +3,11 @@
 
 #include "Ares/Core/Core.h"
 
+#include "Ares/Debug/Log.h"
+
 #include "Ares/Core/Application.h"
 #include "Ares/Core/Timestep.h"
+
+#include "Ares/Events/ApplicationEvent.h"
+#include "Ares/Events/Event.h"
+
