6c671e1eba62ea67d588452310c36686a9fbc9b6
Jack Phillips <jack.p.phillips@gmail.com>
2024-11-10 17:24:04 -0700

    Added layerstack and events to Application class

--- src/Ares/Engine/Core/Application.cpp
+++ src/Ares/Engine/Core/Application.cpp
@@ -21,15 +21,67 @@ namespace Ares {
 
 	}
 
+	void Application::PushLayer(Layer* layer)
+	{
+		m_LayerStack.PushLayer(layer);
+		layer->OnAttach();
+	}
+
+	void Application::PushOverlay(Layer* overlay)
+	{
+		m_LayerStack.PushOverlay(overlay);
+		overlay->OnAttach();
+	}
+
 	void Application::Run()
 	{
+		AR_CORE_INFO("Engine Running...");
 		while (m_Running)
 		{
 			float time = (float)glfwGetTime();
 			Timestep timestep = time - m_LastFrameTime;
 			m_LastFrameTime = time;
 
+			if (!m_Minimized)
+			{
+				for (Layer* layer : m_LayerStack)
+					layer->OnUpdate(timestep);
+			}
+
 			m_Window->OnUpdate();
 		}
 	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(AR_BIND_EVENT_FN(Application::OnWindowClose));
+		dispatcher.Dispatch<WindowResizeEvent>(AR_BIND_EVENT_FN(Application::OnWindowResize));
+
+		for (auto it = m_LayerStack.rbegin(); it != m_LayerStack.rend(); ++it)
+		{
+			(*it)->OnEvent(e);
+			if (e.Handled)
+				break;
+		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
+	}
+
+	bool Application::OnWindowResize(WindowResizeEvent& e)
+	{
+		if (e.GetWidth() == 0 || e.GetHeight() == 0)
+		{
+			m_Minimized = true;
+			return false;
+		}
+
+		m_Minimized = false;
+		return false;
+	}
+
 }
--- src/Ares/Engine/Core/Application.h
+++ src/Ares/Engine/Core/Application.h
@@ -3,6 +3,9 @@
 #include "Engine/Core/Core.h"
 
 #include "Engine/Core/Window.h"
+#include "Engine/Events/ApplicationEvent.h"
+#include "Engine/Events/Event.h"
+#include "Engine/Layers/LayerStack.h"
 
 int main(int argc, char** argv);
 
@@ -14,17 +17,25 @@ namespace Ares {
 		Application();
 		virtual ~Application();
 
+		void OnEvent(Event& e);
+
+		void PushLayer(Layer* layer);
+		void PushOverlay(Layer* overlay);
+
 		inline Window& GetWindow() { return *m_Window; }
 
 		inline static Application& Get() { return *s_Instance; }
 
 	private:
 		void Run();
+		bool OnWindowClose(WindowCloseEvent& e);
+		bool OnWindowResize(WindowResizeEvent& e);
 	
 	private:
 		Scope<Window> m_Window;
 		bool m_Running = true;
 		bool m_Minimized = false;
+		LayerStack m_LayerStack;
 		float m_LastFrameTime = 0.0f;
 
 	private:
