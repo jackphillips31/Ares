daa84588132ef45f8f26d15073315c2cc60ab84c
Jack Phillips <jack.p.phillips@gmail.com>
2024-11-28 21:51:54 -0700

    Updated scripts.

--- scripts/InitializeProject.bat
+++ scripts/InitializeProject.bat
@@ -6,6 +6,10 @@ set TemplateFile=template.lua
 set OutputFile=..\premake5.lua
 set PremakeExe=premake5.exe
 
+:: Define time variable
+set CompleteTime=""
+set OnStep=0
+
 :: Check if template.lua exists
 if not exist "%TemplateFile%" (
 	echo Error: %TemplateFile% does not exist in the current folder.
@@ -30,14 +34,38 @@ echo What is the internal development name of your project?
 set /p InternalName=
 cls
 
+:: Get time for premake file
+set OnStep=0
+goto GetDateAndTime
+:datetime_step0
+
+:: Get time from template
+set TemplateLineNumber=0
+set TemplateDateTime=
+for /f "usebackq delims=" %%A in ("%TemplateFile%") do (
+	set /a TemplateLineNumber+=1
+	if !TemplateLineNumber! equ 2 (
+		set Line=%%A
+		set TemplateDateTime=!Line:-- =!
+		goto template_time_retrieved
+	)
+)
+:template_time_retrieved
+
 :: Read and replace placeholders in template.lua
 echo Processing template...
+echo -- premake5.lua was generated on: %CompleteTime% > "%OutputFile%"
+echo -- Using template.lua from:       %TemplateDateTime% >> "%OutputFile%"
+set Counter=0
 (for /f "usebackq delims=" %%A in ("%TemplateFile%") do (
-    set Line=%%A
-    set Line=!Line:TEMPLATENAME=%ProjectName%!
-    set Line=!Line:TEMPLATEINTERNALNAME=%InternalName%!
-    echo !Line!
-)) > "%OutputFile%"
+	set /a Counter+=1
+	if !Counter! gtr 2 (
+		set Line=%%A
+		set Line=!Line:TEMPLATENAME=%ProjectName%!
+		set Line=!Line:TEMPLATEINTERNALNAME=%InternalName%!
+		echo !Line!
+	)
+)) >> "%OutputFile%"
 
 echo Template processed. Created "%OutputFile%".
 
@@ -61,10 +89,17 @@ echo Select the development environment from the list below:
 %PremakeExe% --help | findstr /r /c:"^ [a-z]"
 set /p DevEnv=Enter the environment name:
 
+:: Get time for batch script
+set OnStep=1
+goto GetDateAndTime
+:datetime_step1
+
 :: Create the batch script for Premake
 cls
 set PremakeScript=GenerateProjectFiles.bat
-echo @echo off > "%PremakeScript%"
+echo :: GenerateProjectFiles.bat was generated on: > "%PremakeScript%"
+echo :: %CompleteTime% >> "%PremakeScript%"
+echo @echo off >> "%PremakeScript%"
 echo pushd %%~dp0\..\ >> "%PremakeScript%"
 echo call scripts\premake5.exe %DevEnv% >> "%PremakeScript%"
 echo popd >> "%PremakeScript%"
@@ -88,3 +123,112 @@ if /i "!RunScript!"=="y" (
 	echo Invalid input. Please enter 'y' or 'n'.
 	goto ask_run_script
 )
+
+exit /b 0
+
+:GetDateAndTime
+:: Fetch the raw output of wmic and parse it
+for /f "skip=1 tokens=1 delims=." %%A in ('wmic os get localdatetime') do (
+    set "DateTime=%%A"
+    goto :ParseDateTime
+)
+
+:ParseDateTime
+:: Parse the DateTime string into components
+set "YYYY=%DateTime:~0,4%"
+set "MM=%DateTime:~4,2%"
+set "DD=%DateTime:~6,2%"
+set "HH=%DateTime:~8,2%"
+set "MI=%DateTime:~10,2%"
+
+:: Adjust the hour for UTC (Arizona is UTC-7)
+set /a HH=1%HH%-100+7
+if %HH% geq 24 (
+    set /a HH-=24
+    set /a DD+=1
+)
+if %HH% lss 0 (
+    set /a HH+=24
+    set /a DD-=1
+)
+
+:: Add leading zero to single-digit hours
+if %HH% lss 10 set HH=0%HH%
+
+:: Determine days in the current month
+set "DaysInMonth=31"
+if %MM%==04 set "DaysInMonth=30"
+if %MM%==06 set "DaysInMonth=30"
+if %MM%==09 set "DaysInMonth=30"
+if %MM%==11 set "DaysInMonth=30"
+if %MM%==02 (
+	:: Check for leap year
+	set "DaysInMonth=28"
+	set /a LeapYear=YYYY %% 4
+	if !LeapYear!==0 (
+		set /a LeapYear=YYYY %% 100
+		if not !LeapYear!==0 set "DaysInMonth=29"
+		set /a LeapYear=YYYY %% 400
+		if !LeapYear!==0 set "DaysInMonth=29"
+	)
+)
+
+:: Handle month/year rollover for increasing day
+if %DD% gtr %DaysInMonth% (
+	set DD=1
+	set /a MM+=1
+	if !MM! gtr 12 (
+		set MM=01
+		set /a YYYY+=1
+	)
+)
+
+:: Handle month/year rollover for decreasing day
+if %DD% lss 1 (
+	set /a MM-=1
+	if !MM! lss 1 (
+		set MM=12
+		set /a YYYY-=1
+	)
+	:: Recalculate days in the new month
+	set "DaysInMonth=31"
+	if %MM%==04 set "DaysInMonth=30"
+  if %MM%==06 set "DaysInMonth=30"
+  if %MM%==09 set "DaysInMonth=30"
+  if %MM%==11 set "DaysInMonth=30"
+	if !MM!==2 (
+		set "DaysInMonth=28"
+		set /a LeapYear=YYYY %% 4
+		if !LeapYear!==0 (
+			set /a LeapYear=YYYY %% 100
+			if not !LeapYear!==0 set "DaysInMonth=29"
+			set /a LeapYear=YYYY %% 400
+			if !LeapYear!==0 set "DaysInMonth=29"
+		)
+	)
+	set DD=%DaysInMonth%
+)
+
+:: Convert numeric month to abbreviated month name
+for %%M in (Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec) do (
+	set /a Index+=1
+	if !Index! equ %MM% set "Month=%%M"
+)
+
+:: Convert hour from 24-hour format to 12-hour format
+set /a Hour12=%HH% %% 12
+if %Hour12%==0 set Hour12=12
+set "AMPM=AM"
+if %HH% geq 12 set "AMPM=PM"
+if %Hour12% lss 10 set "Hour12=0%Hour12%"
+
+:: Format the date and time
+set "FormattedDate=%Month%. %DD%, %YYYY%"
+set "FormattedTime=%Hour12%:%MI%%AMPM% UTC"
+set "CompleteTime=%FormattedDate% - %FormattedTime%"
+
+if !OnStep!==0 goto datetime_step0
+if !OnStep!==1 goto datetime_step1
+
+endlocal
+exit /b 0
--- scripts/template.lua
+++ scripts/template.lua
@@ -1,49 +1,41 @@
+-- template.lua was generated on: 
+-- Nov. 29, 2024 - 04:27AM UTC 
 workspace "TEMPLATENAME"
 	architecture "x86_64"
-
 	configurations
 	{
 		"Debug",
 		"Release",
 		"Dist"
 	}
-
 	startproject "TEMPLATEINTERNALNAME"
-
 	outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-
 	-- Include directories relative to root folder (solution directory)
 	IncludeDir = {}
 	IncludeDir["spdlog"] = "src/Vendor/spdlog/include"
 	IncludeDir["GLAD"] = "src/Vendor/GLAD/include"
 	IncludeDir["ImGui"] = "src/Vendor/imgui"
 	IncludeDir["glm"] = "src/Vendor/glm"
-
 	group "Dependencies"
 		include "src/Vendor/GLAD"
 		include "src/Vendor/imgui"
 		include "src/Vendor/spdlog"
 	group ""
-
 	project "Ares"
 		location "src/Ares"
 		kind "StaticLib"
 		language "C++"
 		cppdialect "C++20"
 		staticruntime "on"
-
 		targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
 		objdir ("%{wks.location}/bin/" ..outputdir.. "/%{prj.name}/obj")
-
 		pchheader "arespch.h"
 		pchsource "src/%{prj.name}/arespch.cpp"
-
 		files
 		{
 			"src/%{prj.name}/**.h",
 			"src/%{prj.name}/**.cpp"
 		}
-
 		includedirs
 		{
 			"src/%{prj.name}",
@@ -52,7 +44,6 @@ workspace "TEMPLATENAME"
 			"%{IncludeDir.ImGui}",
 			"%{IncludeDir.glm}"
 		}
-
 		links
 		{
 			"GLAD",
@@ -60,48 +51,39 @@ workspace "TEMPLATENAME"
 			"spdlog",
 			"opengl32.lib"
 		}
-
 		filter "system:windows"
 			systemversion "latest"
-
 			defines
 			{
 				"AR_PLATFORM_WINDOWS",
 				"AR_BUILD_DLL",
 				"IMGUI_IMPL_OPENGL_LOADER_GLAD"
 			}
-
 		filter "configurations:Debug"
 			defines "AR_DEBUG"
 			runtime "Debug"
 			symbols "on"
-
 		filter "configurations:Release"
 			defines "AR_RELEASE"
 			runtime "Release"
 			optimize "on"
-
 		filter "configurations:Dist"
 			defines "AR_DIST"
 			runtime "Release"
 			optimize "on"
-
 	project "TEMPLATEINTERNALNAME"
 		location "src/TEMPLATEINTERNALNAME"
 		kind "WindowedApp"
 		language "C++"
 		cppdialect "C++20"
 		staticruntime "on"
-
 		targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
 		objdir ("%{wks.location}/bin/" ..outputdir.. "/%{prj.name}/obj")
-
 		files
 		{
 			"src/%{prj.name}/**.h",
 			"src/%{prj.name}/**.cpp"
 		}
-
 		includedirs
 		{
 			"src/Ares",
@@ -109,35 +91,28 @@ workspace "TEMPLATENAME"
 			"%{IncludeDir.spdlog}",
 			"%{IncludeDir.glm}"
 		}
-
 		links
 		{
 			"Ares"
 		}
-
 		filter "system:windows"
 			systemversion "latest"
-
 			defines
 			{
 				"AR_PLATFORM_WINDOWS"
 			}
-
 			files 
 			{
 				"src/%{prj.name}/**.rc"
 			}
-
 		filter "configurations:Debug"
 			defines "AR_DEBUG"
 			runtime "Debug"
 			symbols "on"
-
 		filter "configurations:Release"
 			defines "AR_RELEASE"
 			runtime "Release"
 			optimize "on"
-
 		filter "configurations:Dist"
 			defines "AR_DIST"
 			runtime "Release"
